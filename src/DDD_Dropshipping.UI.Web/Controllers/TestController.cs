7c06cf334f6a332d6dc88ac9e77d62b54cec03bb
Adam Maliszewski <Adam481@autograf.pl>
2018-02-04 20:47:58 +0100

    Created: Domain project in each service. Added: Test controller in each service api

---
+++ src/DDD_Dropshipping.UI.Web/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DDD_Dropshipping.UI.Web.Controllers
+{
+    [Route("api/test")]
+    [Produces("application/json")]
+    public class TestController : Controller
+    {
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get()
+        {
+            return new JsonResult("TEST gateway");
+        }
+    }
+}
