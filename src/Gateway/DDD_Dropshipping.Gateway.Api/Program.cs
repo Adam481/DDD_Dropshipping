2c09fdda926e66e6f30742d31109b72e49aab21a
Adam Maliszewski <Adam481@autograf.pl>
2018-02-05 23:09:36 +0100

    Api gateway - base working ocelot configuration has been added.

---
+++ src/Gateway/DDD_Dropshipping.Gateway.Api/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DDD_Dropshipping.Gateway.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            IWebHostBuilder builder = WebHost.CreateDefaultBuilder(args);
+
+            //  Inject the IWebHostBuilder interface to get the applications scheme, url and port for ocelot
+            builder.ConfigureServices(s =>
+            {
+                s.AddSingleton(builder);
+            });
+
+            // Add ocelot configuration file
+            builder.ConfigureAppConfiguration((builderContext, config) =>
+            {
+                config.AddJsonFile("configuration.json");
+            });
+
+            // The rest is default
+            return builder.UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
---
+++ src/Gateway/DDD_Dropshipping.Gateway.Api/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace DDD_Dropshipping.Gateway.Api
+{
+    // http://localhost:52001/ - Gateway
+    // http://localhost:52002/ - Ordering service
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            
+
+            app.UseOcelot().Wait();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
